420c7b1430aff4056d8196efed0aafee0bf6fc19
estebansant <estebansant30@gmail.com>
2023-03-28 11:59:44 -0300

    dynamic classes in vue

--- practice/app.js
+++ practice/app.js
@@ -1,11 +1,11 @@
 new Vue({
   el: '#app',
+
   data() {
     return{
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: 10,
-      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
       pricesWithDays: [
         { day: 'Monday', value: 8400 },
         { day: 'Tuesday', value: 7900 },
@@ -14,7 +14,15 @@ new Vue({
         { day: 'Friday', value: 9400 },
         { day: 'Saturday', value: 10000 },
         { day: 'Sunday', value: 10200 },
-      ]
+      ],
+      showPrices: false,
+      price: 8400,
+    }
+  },
+
+  methods: {
+    toggleShowPrices () {
+      this.showPrices = !this.showPrices;
     }
   }
 })
